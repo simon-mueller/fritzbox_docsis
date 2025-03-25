0d635c2c928cec326b2c588d3f40f51c3d28343f
Simon Müller <git@simon-mueller.de>
2025-03-25 23:49:06 +0100

    fix config validation type

--- custom_components/fritzbox_docsis/sensor.py
+++ custom_components/fritzbox_docsis/sensor.py
@@ -33,8 +33,8 @@ ICON = 'mdi:router-wireless'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
   vol.Required(CONF_FB_HOST): cv.string,
-  vol.Required(CONF_FB_USER): cv. string,
-  vol.Required(CONF_FB_PASS): cv.password,
+  vol.Required(CONF_FB_USER): cv.string,
+  vol.Required(CONF_FB_PASS): cv.string,
 })
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
